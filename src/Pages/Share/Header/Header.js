aafbca9b72bb7ddb6315bafd31e08fb10bc2285f
nasirdiu <nasir.joypurhat@gmail.com>
2022-04-17 20:35:55 +0600

    finally overall done

--- src/Pages/Share/Header/Header.js
+++ src/Pages/Share/Header/Header.js
@@ -8,9 +8,9 @@ import CustomLink from "../CustomLink/CustomLink";
 
 const Header = () => {
   const [user] = useAuthState(auth);
-  const handleSingOut=()=>{
+  const handleSingOut = () => {
     signOut(auth);
-  }
+  };
   return (
     <Navbar sticky="top" collapseOnSelect expand="lg" bg="info">
       <Container>
@@ -30,13 +30,18 @@ const Header = () => {
             <Nav.Link as={CustomLink} to="/blog">
               Blog
             </Nav.Link>
-            {
-            user?<button className="btn btn-link text-light text-decoration-none" onClick={handleSingOut}>Sing Out</button>:
-              
+            {user ? (
+              <button
+                className="btn btn-link text-light text-decoration-none"
+                onClick={handleSingOut}
+              >
+                Sing Out
+              </button>
+            ) : (
               <Nav.Link as={CustomLink} to="/login">
-              Login
-            </Nav.Link>
-            }
+                Login
+              </Nav.Link>
+            )}
             <Nav.Link as={CustomLink} to="/singUp">
               Sing Up
             </Nav.Link>
